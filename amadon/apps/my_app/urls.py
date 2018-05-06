fc3c411bf70fa7e28435766f24340796297d6909
Aria Samandi <ariasamandi@gmail.com>
2018-05-06 15:43:38 -0700

    django_2

--- amadon/apps/my_app/urls.py
+++ amadon/apps/my_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^process$', views.process),
+    url(r'^process/(?P<item>\w+)$', views.process),
     url(r'^checkout$', views.checkout),
 
 ]
--- amadon/apps/my_app/views.py
+++ amadon/apps/my_app/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'my_app/index.html')
-def process(request):
-    request.session['price'] = request.POST['']
+def process(request, item):
+    request.session['item'] = request.POST['']
